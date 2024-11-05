7aaf5cac997061f084ab9e9bb025a94cc80e8354
godguelito <fuegohabbosf@gmail.com>
2024-11-05 22:54:26 +0100

    Creación de la clase asignatura con sus atributos métodos y la colección de asignaturas.

---
+++ proyecto/clases/Asignatura.php
@@ -0,0 +1,50 @@
+<?php
+//Creación de la clase Asignatura con sus atributos correspondientes.
+class Asignatura {
+    private int $id;
+    private $nombre;
+    private $creditos;
+
+    //Método constructor
+    public function __construct(int $id, $nombre, $creditos) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->creditos = $creditos;
+    }
+
+    //Métodos Setters y Getters
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getCreditos() {
+        return $this->creditos;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setCreditos($creditos): void {
+        $this->creditos = $creditos;
+    }
+
+    //Método estático que crea una colección de asignaturas de muestra.
+    public static function crearAsignaturasdeMuestra(): array {
+        $asignatura1 = new Asignatura(1, "DWES", "7 créditos");
+        $asignatura2 = new Asignatura(2, "DWEC", "6 créditos");
+        $asignatura3 = new Asignatura(3, "DIW", "4 créditos");
+        $asignatura4 = new Asignatura(4, "DAW", "4 créditos");
+
+        return [$asignatura1, $asignatura2, $asignatura3, $asignatura4];
+    }
+}
+?>
