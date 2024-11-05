4ebef4ed87f70335880e7c34adae2a288a42f0a1
godguelito <fuegohabbosf@gmail.com>
2024-11-05 21:06:53 +0100

    Creamos los métodos correspondientes, que son el constructor y los getters y setters.

--- proyecto/clases/Miembro.php
+++ proyecto/clases/Miembro.php
@@ -1,10 +1,52 @@
 <?php
 
-abstract class Miembro {
+abstract class Miembro {  //Creación de la clase abstracta Miembro
     protected int $id;
     protected $nombre;
-    protected string $apellidos;
-    protected string $email;
+    protected $apellidos;
+    protected $email;
+
+    //Método constructor
+    public function __construct(int $id, $nombre, $apellidos, $email) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->email = $email;
+    }
+
+    //Getters y setters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getApellidos() {
+        return $this->apellidos;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setApellidos($apellidos) {
+        $this->apellidos = $apellidos;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    
 }
 
 ?>
